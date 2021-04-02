1fe69ba740163ef7f5ac0430d59342be605a413a
Agnasarp <info.agnasarp@gmail.com>
2021-04-02 16:11:07 +0530

    Configured hystrix dashboard.

---
+++ src/test/java/com/agnasarp/hystrixdashboard/AgnasarpHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnasarp.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgnasarpHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
